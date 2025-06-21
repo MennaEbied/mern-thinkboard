8083fc4ba4c21a20aaea43d973d3f666431d49ed
MennaEbied <mennahesham253@gmail.com>
2025-06-21 04:05:29 +0300

    Implement middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,26 @@ import express from 'express'
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
 import dotenv from 'dotenv'
+import rateLimiter from './middleware/rateLimiter.js'
 
 dotenv.config()
 const app = express()
 const port = process.env.Port || 5001
-connectDB()
+
+
+app.use(express.json()) //get access to req.body
+app.use(rateLimiter)
+//custom middlleware
+/* app.use((req, res, next)=>{
+    console.log("We just got a new request")
+    next()
+}) */
 
 app.use('/api/notes',notesRoutes)
 
-app.listen(port,()=>{
-    console.log("server started on http://localhost:",port)
+connectDB().then(() => {
+    app.listen(port,()=>{
+        console.log("server started on http://localhost:",port)
+    })
 })
+
