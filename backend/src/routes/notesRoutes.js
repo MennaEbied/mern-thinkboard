f27a302afb238dfbb4dea36c459c32c0487c3cd6
MennaEbied <mennahesham253@gmail.com>
2025-06-21 03:19:32 +0300

    Update routes

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createNote, deleteNote, getAllNotes, updateNote } from '../controllers/notesControllers.js';
+import { createNote, deleteNote, getAllNotes, updateNote, getNoteById } from '../controllers/notesControllers.js';
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 router.post("/",createNote)
-router.get("/:id", updateNote)
+router.put("/:id", updateNote)
 router.delete("/:id", deleteNote) 
 
 export default router;
